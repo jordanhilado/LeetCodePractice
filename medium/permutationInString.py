9a893c0798f10158ecb5a2d3921ef96fa4ecb1ff
Jordan Hilado <37820188+jordanhilado@users.noreply.github.com>
2021-12-10 00:41:18 -0800

    perm

---
+++ medium/permutationInString.py
@@ -0,0 +1,24 @@
+# 567. Permutation in String
+# https://leetcode.com/problems/permutation-in-string/
+
+def checkInclusion(self, s1: str, s2: str) -> bool:
+    from collections import Counter
+    k = len(s1)
+    d1 = Counter(s1)
+    
+    window = s2[:k]
+    d2 = Counter(window)
+    if d1 == d2:
+        return True
+    for i in range(len(s2) - k):
+        if d2[s2[i]] == 1:
+            del d2[s2[i]]
+        elif d2[s2[i]] > 1:
+            d2[s2[i]] -= 1
+        if s2[i+k] in d2:
+            d2[s2[i+k]] += 1
+        else:
+            d2[s2[i+k]] = 1
+        if d1 == d2:
+            return True
+    return False
