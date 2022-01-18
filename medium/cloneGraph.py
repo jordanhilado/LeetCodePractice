3dab040f9624a71655f058c6560d9e57f54cd335
Jordan Hilado <jordanalihilado@gmail.com>
2022-01-18 09:38:59 -0800

    ;)

---
+++ medium/cloneGraph.py
@@ -0,0 +1,18 @@
+# 133. Clone Graph
+# https://leetcode.com/problems/clone-graph/
+
+def cloneGraph(self, node: 'Node') -> 'Node':
+    if not node:
+        return
+    d = {node:Node(node.val)}
+    stack = [node]
+    while stack:
+        curr = stack.pop()
+        for i in curr.neighbors:
+            if i not in d:
+                d[i] = Node(i.val)
+                stack.append(i)
+            d[i].neighbors.append(d[curr])
+    return d[node]
+    copy = Node()
+    print(node.neighbors)
