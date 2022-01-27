522f13e220d37dc1c5159999fb0d1365b20773b5
Jordan Hilado <jordanalihilado@gmail.com>
2022-01-26 19:16:25 -0800

    change combination sum approach

--- medium/combinationSum.py
+++ medium/combinationSum.py
@@ -2,14 +2,16 @@
 # https://leetcode.com/problems/combination-sum/
 
 def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-    ans = []
-    def dfs(target, index, path):
-        if target < 0:
+    res = []
+    def dfs(i, curr, total):
+        if total == target:
+            res.append(curr.copy())
             return
-        if target == 0:
-            ans.append(path)
+        if i >= len(candidates) or total > target:
             return
-        for i in range(index, len(candidates)):
-            dfs(target - candidates[i], i, path + [candidates[i]])
-    dfs(target, 0, [])
-    return ans
+        curr.append(candidates[i])
+        dfs(i, curr, total + candidates[i])
+        curr.pop()
+        dfs(i + 1, curr, total)
+    dfs(0, [], 0)
+    return res
