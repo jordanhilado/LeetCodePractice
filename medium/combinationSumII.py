f0e86c04f6c266483124b1bf63bed799982ca438
Jordan Hilado <jordanalihilado@gmail.com>
2022-02-01 00:27:24 -0800

    update combination sum II solutions

--- medium/combinationSumII.py
+++ medium/combinationSumII.py
@@ -1,6 +1,8 @@
 # 40. Combination Sum II
 # https://leetcode.com/problems/combination-sum-ii/
 
+#################### Backtracking Solution ####################
+
 def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
     def dfs(nums, target, index, path, ans):
         if target <= 0:
@@ -14,3 +16,16 @@ def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]
     ans = []
     dfs(sorted(candidates), target, 0, [], ans)
     return ans
+
+#################### DP Solution ####################
+
+def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+    candidates.sort()
+    table = [None] + [set() for i in range(target)]
+    for i in candidates:
+        if i > target:
+            break
+        for j in range(target - i, 0, -1):
+            table[i + j] |= {elt + (i,) for elt in table[j]}
+        table[i].add((i,))
+    return map(list, table[target])
