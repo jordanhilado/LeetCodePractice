451c480cb576799f9fc19d23429edc4476ba71b7
Jordan Hilado <jordanalihilado@gmail.com>
2022-03-05 18:56:12 -0800

    dp problem

---
+++ medium/uglyNumberII.py
@@ -0,0 +1,17 @@
+# 264. Ugly Number II
+# https://leetcode.com/problems/ugly-number-ii/
+
+def nthUglyNumber(self, n: int) -> int:
+    if n == 1: return 1
+    x = y = z = 0
+    dp = [1] + [0] * (n - 1)
+    for i in range(1, n):
+        dp[i] = min(dp[x] * 2, dp[y] * 3, dp[z] * 5)
+        if dp[i] == dp[x] * 2:
+            x += 1
+        if dp[i] == dp[y] * 3:
+            y += 1
+        if dp[i] == dp[z] * 5:
+            z += 1
+        print(dp)
+    return dp[-1]
